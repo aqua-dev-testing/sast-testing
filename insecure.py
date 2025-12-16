ad55cee978d97530d0d9bd552eeb1d7f10297cff
Fatima <66124862+fatima99s@users.noreply.github.com>
2025-12-16 13:36:35 +0000

    Add file reading and unpickling functionality
    Added code to read and unpickle data from a file.

--- insecure.py
+++ insecure.py
@@ -2,3 +2,6 @@ API_KEY = "sk_live_1234567890abcdef"  # hardcoded secret
 
 def calculate(expr):
     return eval(expr)  # dangerous eval
+
+with open(reading_path, "rb") as file:
+    data = pickle.load(file)
